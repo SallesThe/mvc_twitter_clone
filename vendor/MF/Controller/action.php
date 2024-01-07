1edec5c6a93ff7195e5f00711e1160819dfe69f6
SallesThe <manodada26@gmail.com>
2024-01-07 16:36:36 -0300

    configurating abstract class controller

---
+++ vendor/MF/Controller/action.php
@@ -0,0 +1,32 @@
+<?php 
+    namespace MF\Controller;
+    abstract class Action
+    {
+        private $view;
+
+        public function __construct() 
+        {
+            $this->view = new \stdClass;
+        }
+        
+        public function render($view, $layout = 'layout'): void
+        {
+            $this->view->page = $view;
+            if(file_exists("../App/View/$layout.phtml"))
+            {
+                require_once "../App/View/$layout.phtml";
+            } else {
+                $this->content();
+            }
+        }
+
+        protected function content(): void
+        {
+            $currentClass = get_class($this);
+            $currentClass = str_replace('App\\Controller','',$currentClass);
+            $currentClass = str_replace('Controller','',$currentClass);
+            $currentClass = str_replace('\\','',$currentClass);
+
+            require_once "../App/View/$currentClass/" . $this->view->page . ".phtml";
+        }
+    }
